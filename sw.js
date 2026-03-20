c9cdd1587a2d99bf9b785496db0523391c40f492
ilayda <99603563+illaydayldz@users.noreply.github.com>
2026-03-20 03:10:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,12 @@
-const CACHE='froguary-v2';
-const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
-
-self.addEventListener('install', e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch', e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
-
-// Bildirim mesajı al
-self.addEventListener('message', e=>{
-  if(e.data && e.data.type==='SCHEDULE_NOTIFICATION'){
-    const {title, body, delay} = e.data;
+const CACHE='froguary-v3';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-152.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('message',e=>{
+  if(e.data&&e.data.type==='SCHEDULE_NOTIFICATION'){
+    const {title,body,delay}=e.data;
     setTimeout(()=>{
-      self.registration.showNotification(title, {
-        body,
-        icon: './icons/icon-192.png',
-        badge: './icons/icon-192.png',
-        vibrate: [200, 100, 200],
-        tag: 'froguary-reminder'
-      });
-    }, delay || 0);
+      self.registration.showNotification(title,{body,icon:'./icon-192.png',badge:'./icon-192.png',vibrate:[200,100,200],tag:'froguary-reminder'});
+    },delay||0);
   }
 });
