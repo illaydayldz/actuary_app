9c75e83ce79e39740b600d20e7687267104d5322
ilayda <99603563+illaydayldz@users.noreply.github.com>
2026-03-20 03:42:47 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 const CACHE='froguary-v3';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-152.png'];
+const ASSETS=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-152.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
 self.addEventListener('message',e=>{
   if(e.data&&e.data.type==='SCHEDULE_NOTIFICATION'){
     const {title,body,delay}=e.data;
     setTimeout(()=>{
-      self.registration.showNotification(title,{body,icon:'./icon-192.png',badge:'./icon-192.png',vibrate:[200,100,200],tag:'froguary-reminder'});
+      self.registration.showNotification(title,{body,icon:'./icons/icon-192.png',badge:'./icons/icon-192.png',vibrate:[200,100,200],tag:'froguary-reminder'});
     },delay||0);
   }
 });
